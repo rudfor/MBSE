57d293d666b0c8f460a4c1e3a346ba2b983a1c03
rudolf anton fortes <ruft@demant.com>
2022-10-23 22:23:34 +0200

    bug fix on initial example

--- main.py
+++ main.py
@@ -13,17 +13,16 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    #ox.plot_graph(ox.graph_from_place('Amager, Denmark'))
-    G = ox.graph_from_place('Santa Monica, California', network_type='walk')
-    basic_stats = ox.basic_stats(G)
-    print(basic_stats['circuity_avg'])
-    extended_stats = ox.extended_stats(G, bc=True)
-    print(extended_stats['betweenness_centrality_avg'])
+    ox.plot_graph(ox.graph_from_place('Amager, Denmark'))
+    # G = ox.graph_from_place('Amager, Denmark', network_type='walk')
+    # basic_stats = ox.basic_stats(G)
+    # print(basic_stats['circuity_avg'])
+    # extended_stats = ox.extended_stats(G, bc=True)
+    # print(extended_stats['betweenness_centrality_avg'])
 
+    # s = pd.Series([1, 2, 3])
+    # fig, ax = plt.subplots()
+    # s.plot.bar()
+    # fig.savefig('my_plot.png')
 
-#    s = pd.Series([1, 2, 3])
-#    fig, ax = plt.subplots()
-#    s.plot.bar()
-#    fig.savefig('my_plot.png')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
