6296a817bcbdb1682de55abaae14f9bac40717e2
Andreas Breum <s176334@student.dtu.dk>
2022-10-28 11:12:22 +0200

    drone and bicycle class added

--- system/drone.py
+++ system/drone.py
@@ -1,10 +1,15 @@
+from turtle import position
 from system.courier import Courier
 
 
-class Drone(Courier):
+class Drone_type1(Courier):
+# https://uavsystemsinternational.com/products/tarot-t-18-ready-fly-drone/
+    
     def __init__(self, position):
         super().__init__(position)
-        self.battery = 100
-
-    def move(self):
-        pass
+        self.cost = 38062 # DKK
+        self.battery = 25 # min
+        # self.range = 3200 # m
+        self.charge_time = 60 # 60 - 90 min
+        self.avg_speed = 15 # m/s
+        self.cargo_weight = 8 # kg
