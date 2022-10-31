ad0b91771f34860050c33797bab101043722bc25
Christopher Acosta <chriz@live.dk>
2022-10-31 21:24:18 +0100

    WIP: Simulator with OSMNX

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,46 +1,75 @@
 import itertools
 
+from environment.order import Order
+from experiment.test3 import Map
 from system.bike import Bike
-from system.drone import Drone
+from system.drone import Drone_type1
 from system.kitchen import Kitchen
 from environment.order_generator import OrderGenerator
 from utility.point import Point
 
-# Smallest time unit
-time_quantum = 1
-# Number of times to run the simulation (advance the time unit)
-time_quantum_rounds = 1000
+# total_score = 0
 
-total_score = 0
+# order_generator = OrderGenerator()
 
-order_generator = OrderGenerator()
-
-kitchen = Kitchen(Point(0, 0))
-num_drones = 10
-num_bikes = 10
-drones = [Drone(kitchen.position) for _ in range(0, num_drones)]
-bikes = [Bike(kitchen.position) for _ in range(0, num_bikes)]
+# kitchen = Kitchen(Point(0, 0))
+#num_drones = 10
+# drones = [Drone_type1(kitchen.position) for _ in range(0, num_drones)]
+# bikes = [Bike(kitchen.position) for _ in range(0, num_bikes)]
 
 
-def calculate_score(order):
-    pass
+TIME_LIMIT_MINUTES = 60
+ORDER_INTERVAL_MINUTES = 10
+
+MAP = Map()
+
+# 5 km/h
+AVG_BIKE_SPEED_METERS_PER_HOUR = 5000
+
+num_bikes = 1
+bikes = [Bike(Point(45501638, 45521416)) for _ in range(0, num_bikes)]
+
+# def calculate_score(order):
+#     pass
 
 
 def run_simulator():
-    for _ in range(0, time_quantum_rounds):
-        orders = order_generator.advance(1)
+    current_time_minutes = 0
 
-        kitchen.receive_orders(orders)
+    while current_time_minutes <= TIME_LIMIT_MINUTES:
+        current_time_minutes += ORDER_INTERVAL_MINUTES
 
-        couriers = itertools.chain(drones, bikes)
+        if current_time_minutes % ORDER_INTERVAL_MINUTES == 0:
+            # Generate random order destination
+            order_start, order_end = MAP.next_destination()
+            distance = MAP.path_length(order_start, order_end)
+            print(f"order at time {current_time_minutes}: ({order_start}, {order_end}), distance: {distance}")
 
-        for courier in couriers:
-            if kitchen.courier_present(courier):
-                kitchen.pickup_order(courier)
+            # Create bike courier
+            for bike in bikes:
+                if not bike.is_delivering():
+                    bike.order = Order(order_start, order_end, None)
 
-        for courier in couriers:
-            if courier.order_delivered():
-                total_score += calculate_score(courier.order)
+            # Move bike couriers
+            for bike in bikes:
+                bike.move(ORDER_INTERVAL_MINUTES)
 
-        for courier in couriers:
-            courier.move()
+
+            # map.plot_path(order_start, order_end)
+        ######
+        # orders = order_generator.advance(1)
+        #
+        # kitchen.receive_orders(orders)
+        #
+        # couriers = itertools.chain(drones, bikes)
+        #
+        # for courier in couriers:
+        #     if kitchen.courier_present(courier):
+        #         kitchen.pickup_order(courier)
+        #
+        # for courier in couriers:
+        #     if courier.order_delivered():
+        #         total_score += calculate_score(courier.order)
+        #
+        # for courier in couriers:
+        #     courier.move()
