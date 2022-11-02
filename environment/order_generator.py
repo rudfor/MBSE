4ace2c63daf85eb0f030985d5d94968b5eed8dfb
Christopher Acosta <chriz@live.dk>
2022-11-02 14:47:38 +0100

    Fix time until next order + print more info

--- environment/order_generator.py
+++ environment/order_generator.py
@@ -12,10 +12,10 @@ class OrderGenerator:
     # def advance(self, dt):
     #     return []
 
-    def generate_order(self):
+    def generate_order(self, time_ordered):
         order_start, order_end = self.map.next_destination()
         distance = self.map.path_length(order_start, order_end)
-        return Order(Point(order_start, order_end), None, None, distance)
+        return Order(Point(order_start, order_end), time_ordered, None, distance)
 
     def generate_time_until_order(self):
         return numpy.random.normal(loc=10, scale=2.0, size=None)
