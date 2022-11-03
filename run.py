b0afed9058b059ca317d77e95fdda2a960562241
rudolf anton fortes <ruft@demant.com>
2022-11-03 22:04:35 +0100

    added sim_py.py experiment condition to see

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import sim_libs
+import simpy
 
 if __name__ == '__main__':
-    sim_libs.simulator.Simulator(1)
+    env = simpy.Environment()
+    res = simpy.Resource(env, capacity=1)
+    drone = env.process(sim_libs.resource_drone(env, res))
+    procs = [env.process(sim_libs.drone(res)), env.process(sim_libs.drone(res))]
+    env.run(40)
+    #sim_libs.simulator.Simulator(1)
     #simulator.RUN()
     #run_simulator()
--- sim_libs/simulator/run.py
+++ sim_libs/simulator/run.py
@@ -7,6 +7,26 @@ rel_path = os.path.dirname(__file__)
 import simpy
 
 
+def resource_drone(env, resource):
+    request = resource.request()  # Generate a request event
+    yield request                 # Wait for access
+    yield env.timeout(1)          # Do something
+    resource.release(request)     # Release the resource
+
+
+def drone(res):
+    print_stats(res)
+    with res.request() as req:
+        yield req
+        print_stats(res)
+    print_stats()
+
+
+def print_stats(res):
+    print('%d of %d slots are allocated.' % (res.count, res.capacity))
+    print('  Users:', res.drone)
+    print('  Queued events:', res.queue)
+
 class Simulator:
     def __init__(self, sim):
         self.sim = sim
